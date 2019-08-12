9887b2585e60611d267cc995af2c01ef810032dc
Дмитрий Антонов <dim2126@gmail.com>
2019-08-12 23:12:44 +0300

    test #1

---
+++ VisualSorting/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace VisualSorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1068, 550);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 568);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(93, 568);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Shuffle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button3.Location = new System.Drawing.Point(174, 568);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Bubble";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(255, 568);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Merge";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(902, 573);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar1.Size = new System.Drawing.Size(178, 42);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 10;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(877, 578);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 603);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Visual Sorting";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VisualSorting/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualSorting
+{
+    public partial class Form1 : Form
+    {
+        readonly int length = 300; //количество столбцов
+        readonly int columnWidth = 2; //множитель ширины столбцов
+        readonly int columnHeight = 1; //множитель высоты столбцов
+        readonly int interval = 1; // интервал между столбцами
+        readonly int speed_1 = 1; //замедление создания и перемешивания
+        int speed_2 = 10; //замедление сортировки
+
+
+        Rectangle[] rectangles;
+        bool refresh; //отключает обновление pictureBox
+        bool inProgress; //Запрещает запуск других операций во время работы одной
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(length * (columnWidth + interval) + 50, length * columnHeight + 97); // установка размеров формы
+            label1.Text = speed_2.ToString(); //начальное значение лейбла скорости
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            RenderArray(e);
+        }
+
+        private void TrackBar1_ValueChanged(object sender, EventArgs e) //Переключатель скорости сортировки
+        {
+            speed_2 = trackBar1.Value;
+            label1.Text = Convert.ToString(21 - speed_2);
+        } 
+
+        async private void Button1_Click(object sender, EventArgs e) //New
+        {
+            if (inProgress == false)
+            {
+                inProgress = true;
+                NewArrayAsync();
+                for (; ; )
+                {
+                    pictureBox1.Refresh();
+                    if (refresh)
+                        break;
+                    //Thread.Sleep(speed_1);
+                    await Task.Delay(speed_1);
+                }
+                inProgress = false;
+            }
+        }
+
+        async private void Button2_Click(object sender, EventArgs e) //Shuffle
+        {
+            if (inProgress == false)
+            {
+                inProgress = true;
+                ShuffleArrayAsync();
+                for (; ; )
+                {
+                    pictureBox1.Refresh();
+                    if (refresh)
+                        break;
+                    //Thread.Sleep(speed_1);
+                    await Task.Delay(speed_1);
+                }
+                inProgress = false;
+            }
+        }
+
+        async private void Button3_Click(object sender, EventArgs e) //Пузырьковая сортировка
+        {
+            if (inProgress == false)
+            {
+                inProgress = true;
+                SortBubleAsync();
+                for (; ; )
+                {
+                    pictureBox1.Refresh();
+                    if (refresh)
+                        break;
+                    //Thread.Sleep(speed_2);
+                    await Task.Delay(speed_2);
+                }
+                inProgress = false;
+            }
+        }
+
+        async private void Button4_Click(object sender, EventArgs e) //Merge сортировка
+        {
+            if (inProgress == false)
+            {
+                inProgress = true;
+                SortMergeAsync();
+                for (; ; )
+                {
+                    pictureBox1.Refresh();
+                    if (refresh)
+                        break;
+                    //Thread.Sleep(speed_2);
+                    await Task.Delay(speed_2);
+                }
+                inProgress = false;
+            }
+        }
+
+        void NewArray()
+        {
+            if (rectangles == null)
+                rectangles = new Rectangle[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                rectangles[i].Y = 5; //отступ столбцов от верхней границы 
+                //rectangles[i].X = ... //положение столбца на поле назначается при отрисовке
+                rectangles[i].Width = columnWidth; //ввод ширины столбцов
+                rectangles[i].Height = (1 + i) * columnHeight; //ввод высоты столбцов
+                Thread.Sleep(speed_1); //Async
+            }
+            refresh = true;
+        }
+        void ShuffleArray()
+        {
+            if (rectangles != null)
+            {
+                Random rand = new Random();
+                int rand_i;
+                for (int i = 0; i < length; i++)
+                {
+                    rand_i = rand.Next(length - 1);
+                    SwapRect(i, rand_i);
+                    Thread.Sleep(speed_1); //Async
+                }
+            }
+            refresh = true;
+        }
+        void RenderArray(PaintEventArgs e)
+        {
+            if (rectangles != null)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    rectangles[i].X = 10 + i * (columnWidth + interval); //положение столбца на поле
+                    e.Graphics.FillRectangle(Brushes.LightBlue, rectangles[i]);
+                }
+            }
+        }
+        void SortBubble()
+        {
+            if (rectangles != null)
+            {
+                bool isSorted;
+                int k = 0;
+                do
+                {
+                    isSorted = true;
+                    for (int i = 1; i < length - k; i++)
+                    {
+                        if (rectangles[i - 1].Height > rectangles[i].Height)
+                        {
+                            isSorted = false;
+                            SwapRect(i - 1, i);
+                            Thread.Sleep(speed_2);
+                        }
+                    }
+                    k++;
+                }
+                while (isSorted == false);
+            }
+            Thread.Sleep(10); //Что-бы соритровка успела отрендериться
+            refresh = true;
+        }
+        void SortMerge()
+        {
+            if(rectangles != null && rectangles.Length >= 2)
+            {
+                SortMergeDivide(rectangles, 0); 
+            }
+            Thread.Sleep(10);
+            refresh = true;
+
+            void SortMergeDivide(Rectangle[] undivided, int inindex)
+            {
+                Rectangle[] left = new Rectangle[undivided.Length / 2];
+                setLeftRight(0, left.Length, undivided, left);
+                if (left.Length > 1)
+                    SortMergeDivide(left, inindex);
+
+                Rectangle[] right = new Rectangle[undivided.Length - left.Length];
+                setLeftRight(left.Length, undivided.Length, undivided, right);
+                if (right.Length > 1)
+                    SortMergeDivide(right,inindex + left.Length);
+
+                SortMergeMerging(undivided, left, right, inindex);
+            }
+
+            void SortMergeMerging(Rectangle[] merging, Rectangle[] left, Rectangle[] right, int inIndex)
+            {
+                int l = 0;
+                int r = 0;
+                int m = 0;
+                for(int i = 0; i < merging.Length; i++)
+                {
+                    if (l < left.Length && r < right.Length)
+                    {
+                        if (left[l].Height < right[r].Height)
+                        {
+                            merging[i] = left[l];
+                            l++;
+                        }
+                        else
+                        {
+                            merging[i] = right[r];
+                            r++;
+                        }
+                    }
+                    else if (l < left.Length)
+                    {
+                        merging[i] = left[l];
+                        l++;
+                    }
+                    else
+                    {
+                        merging[i] = right[r];
+                        r++;
+                    }
+                    if (merging == rectangles) //Для отображения в реальном времени последнего прохода
+                        Thread.Sleep(speed_2);
+                }
+                for(int i = inIndex; i < inIndex + left.Length + right.Length; i++) //Цикл для отображения сортировки в реальном времени
+                {
+                    rectangles[i] = merging[m];
+                    m++;
+                    Thread.Sleep(speed_2); //Async
+
+                }
+
+            }
+
+            void setLeftRight (int inIndex, int outIndex, Rectangle[] undivided, Rectangle[] leftRight) 
+            {
+                int lR = 0;
+                for(int i = inIndex; i < outIndex; i++)
+                {
+                    leftRight[lR] = undivided[i];
+                    lR++;
+                }
+            }
+        }
+        void SwapRect(int i, int j) //меняет местами 2 элемента в массиве
+        {
+            Rectangle temp;
+            temp = rectangles[i];
+            rectangles[i] = rectangles[j];
+            rectangles[j] = temp;
+        }        
+
+        async void NewArrayAsync()
+        {
+            refresh = false;
+            await Task.Run(() => NewArray());
+        }
+        async void ShuffleArrayAsync()
+        {
+            refresh = false;
+            await Task.Run(() => ShuffleArray());
+        }
+        async void SortBubleAsync()
+        {
+            refresh = false;
+            await Task.Run(() => SortBubble());
+        }
+        async void SortMergeAsync()
+        {
+            refresh = false;
+            await Task.Run(() => SortMerge());
+        }
+
+
+    }
+}
